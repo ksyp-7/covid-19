2e0223301028cebcf01ea1e5e60b62da63406083
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-21 12:44:25 +0530

    Rendered all 6 cards
    implemented all 6 cards with api data

--- src/App.js
+++ src/App.js
@@ -1,54 +1,137 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck'
 
+
 function App() {
+  const [latest, setLatest] = useState("");
+  const [ res,setRes ]= useState("");
+  
+  useEffect(() => {
+    axios
+    .get("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "e5e2f726e4msha90e808df4f0c85p12b883jsn0099c0cb1a3c",
+        "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com"
+      }
+    })
+    .then(response => {
+      setLatest(response.data.world_total);
+      setRes(response.data.countries_stat);
+      console.log(response.data.countries_stat);
+      console.log(response.data.world_total);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+    
+  }, [])
   return (
     <div>
 
       <CardDeck>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+        <Card 
+          bg="secondary"
+          text="white"
+          className="text-center"
+          style={{margin: "10px"}}>
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Total Cases</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              {latest.total_cases}
       </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small>Last updated on {latest.statistic_taken_at}</small>
           </Card.Footer>
         </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+        <Card 
+          bg="dark"
+          text="white" 
+          className="text-center"
+          style={{margin: "10px"}}>
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Total Deaths</Card.Title>
             <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-        content.{' '}
+            {latest.total_deaths}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small>Last updated on {latest.statistic_taken_at}</small>
           </Card.Footer>
         </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+        <Card 
+          bg="success" 
+          text="white" 
+          className="text-center"
+          style={{margin: "10px"}}>
+
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Total Recoverd</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
+            {latest.total_recovered}
       </Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small>Last updated on {latest.statistic_taken_at}</small>
           </Card.Footer>
         </Card>
       </CardDeck>
+      <CardDeck>
+      <Card 
+          bg="danger" 
+          text="white" 
+          className="text-center"
+          style={{margin: "10px"}}>
+
+          <Card.Body>
+            <Card.Title>Total Active Cases</Card.Title>
+            <Card.Text>
+            {latest.active_cases}
+      </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated on {latest.statistic_taken_at}</small>
+          </Card.Footer>
+        </Card>
+        <Card 
+          bg="dark" 
+          text="white" 
+          className="text-center"
+          style={{margin: "10px"}}>
+
+          <Card.Body>
+            <Card.Title>New Deaths</Card.Title>
+            <Card.Text>
+            {latest.new_deaths}
+      </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated on {latest.statistic_taken_at}</small>
+          </Card.Footer>
+        </Card>
+        <Card 
+          bg="warning" 
+          text="white" 
+          className="text-center"
+          style={{margin: "10px"}}>
+
+          <Card.Body>
+            <Card.Title>New cases</Card.Title>
+            <Card.Text>
+            {latest.new_cases}
+      </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated on {latest.statistic_taken_at}</small>
+          </Card.Footer>
+        </Card>
+        </CardDeck>
+
     </div>
   );
 }
