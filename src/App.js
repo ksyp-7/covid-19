a3859896f233ef330404f9699f155c91f1339507
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-22 12:26:46 +0530

    Search function implemented
    Now All things rendring cards,map,country information and Search bar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
-import GoogleMapReact from 'google-map-react';
-import CardColumn from 'react-bootstrap/CardColumns';
+import GoogleMapReact from 'google-map-react'
 import Columns from 'react-columns';
+import Form from 'react-bootstrap/Form';
 
 
 
 function App() {
   const [latest, setLatest] = useState([]);
   const [res, setRes] = useState([]);
+  const [serch, setSerch] = useState("");
 
   useEffect(() => {
     axios
@@ -32,11 +33,46 @@ function App() {
 
   }, [])
 
+
+  const FC = res.filter(item => {
+    return serch !== "" ? item.country.includes(serch) : item
+  })
+  const contries = FC.map((data, i) => {
+    return (
+      <Card
+        key={i}
+        bg="light"
+        text="dark"
+        className="text-center"
+        style={{ margin: "10px" }}>
+        <Card.Img src={data.countryInfo.flag} />
+
+        <Card.Body>
+          <Card.Title>{data.country}</Card.Title>
+          <Card.Text>
+            Total Cases ==>{data.cases}
+            <br />
+      Total Deaths ==>{data.deaths}
+            <br />
+      Active Cases ==>{data.active}
+            <br />
+      Recovered ==>{data.recovered}
+            <br />
+      New Cases ==>{data.todayCases}
+            <br />
+      New Daths ==>{data.todayDeaths}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+
+  });
+
   const date = new Date(parseInt(latest.updated));
   const DateUpdated = date.toString();
 
-  const countryLocation = res.map((data,i) => {
-    return(
+  const countryLocation = res.map((data, i) => {
+    return (
       <div
         key={i}
         lat={data.countryInfo.lat}
@@ -49,50 +85,20 @@ function App() {
           textAlign: "center",
           borderRadius: "30px"
         }}>
-          <img height="10px" src={data.countryInfo.flag} />
-          <br />
-          {data.cases}
-        </div>
+        <img height="10px" src={data.countryInfo.flag} alt="A" />
+        <br />
+        {data.cases}
+      </div>
     )
   })
 
-const cardrender = res.map((data,i) => {
-  return(
-    <Card
-      key={i}
-      bg="light"
-      text="dark"
-      className="text-center"
-      style={{margin: "10px"}}>
-    <Card.Img src={data.countryInfo.flag}/>
-
-    <Card.Body>
-      <Card.Title>{data.country}</Card.Title>
-      <Card.Text>
-      Total Cases ==>{data.cases}
-      <br />
-      Total Deaths ==>{data.deaths}
-      <br />
-      Active Cases ==>{data.active}
-      <br />
-      Recovered ==>{data.recovered}
-      <br />
-      New Cases ==>{data.todayCases}
-      <br />
-      New Daths ==>{data.todayDeaths}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  )
-})
-
-var queries = [{
-  columns: 2,
-  query: 'min-width: 500px'
-}, {
-  columns: 3,
-  query: 'min-width: 1000px'
-}];
+  var queries = [{
+    columns: 2,
+    query: 'min-width: 500px'
+  }, {
+    columns: 3,
+    query: 'min-width: 1000px'
+  }];
 
   return (
     <div>
@@ -202,13 +208,22 @@ var queries = [{
           defaultCenter={{ lat: 20, lng: 77 }}
           defaultZoom={4}
         >
-        {countryLocation}
+          {countryLocation}
         </GoogleMapReact>
+        <br />
+        <Form>
+          <Form.Group controlId="formGroupSearch">
+            <Form.Control
+              type="text"
+              placeholder="Search a country"
+              onChange={e => setSerch(e.target.value)} />
+          </Form.Group>
+        </Form>
 
-       <Columns queries={queries}>
-       {cardrender}
-       </Columns>
-       
+        <Columns queries={queries}>
+          {contries}
+        </Columns>
+
 
       </div>
 
